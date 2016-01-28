4d3e69e89ef57e2033f7d3a5145d6076201f0396
Przemysław Gajdziński <gajdzik11@gmail.com>
2016-01-28 11:31:32 +0100

    dodanie wektoru, aby nie bylo bledu kompilacji przy zapisie do png

--- opencv.cpp
+++ opencv.cpp
@@ -8,6 +8,11 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
+	//wektor aby dzia³a³ zapis do png (inaczej b³ad kompilacji)
+	vector<int>moja;
+	moja.push_back(CV_IMWRITE_PNG_COMPRESSION);
+	moja.push_back(3);
+	
 	//potrzebne dane do Sobel
 	int scale = 1;
 	int delta = 0;
@@ -23,12 +28,12 @@ int main(int argc, char **argv)
 	//konwersja przestrzeni barw na szaroœæ i zapis do grayscale.png
 	Mat gray_image;
 	cvtColor(image, gray_image, CV_BGR2GRAY);
-	imwrite("grayscale.png", gray_image);
+	imwrite("grayscale.png", gray_image,moja);
 
 	//zastosowanie filtru Gaussa 3x3 i zapis do gauss.png
 	Mat gaussian_image;
 	GaussianBlur(gray_image, gaussian_image, Size(3, 3), 1.5, 0);
-	imwrite("gauss.png", gaussian_image);
+	imwrite("gauss.png", gaussian_image,moja);
 
 	//zastosowanie filtru Sobel i zapis do sobel.png
 	Mat sobel_image;
@@ -39,7 +44,7 @@ int main(int argc, char **argv)
 	Sobel(gaussian_image, sobely, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT);
 	convertScaleAbs(sobely, sobely);
 	addWeighted(sobelx, 0.5, sobely, 0.5, 0, sobel_image);
-	imwrite("sobel.png", sobel_image);
+	imwrite("sobel.png", sobel_image,moja);
 
 
 	return 0;
